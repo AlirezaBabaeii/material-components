1f10414f1bda7ebf226de901ef5ed5e23d98c2a7
mikoloism <mikoloism.github@gmail.com>
2023-01-15 14:13:58 +0330

    feat(theme): create theme provider and theme tokens;
    create theme provider by `styled-components` theme provider and `material-design 3` theme builder (theme tokens from `https://m3.material.io/theme-builder#/custom`);

---
+++ src/theme/theme.dark.ts
@@ -0,0 +1,43 @@
+import { token } from './tokens';
+
+export const dark = {
+  'md-sys-color-primary': token['md-sys-color-primary-dark'],
+  'md-sys-color-on-primary': token['md-sys-color-on-primary-dark'],
+  'md-sys-color-primary-container':
+    token['md-sys-color-primary-container-dark'],
+  'md-sys-color-on-primary-container':
+    token['md-sys-color-on-primary-container-dark'],
+  'md-sys-color-secondary': token['md-sys-color-secondary-dark'],
+  'md-sys-color-on-secondary': token['md-sys-color-on-secondary-dark'],
+  'md-sys-color-secondary-container':
+    token['md-sys-color-secondary-container-dark'],
+  'md-sys-color-on-secondary-container':
+    token['md-sys-color-on-secondary-container-dark'],
+  'md-sys-color-tertiary': token['md-sys-color-tertiary-dark'],
+  'md-sys-color-on-tertiary': token['md-sys-color-on-tertiary-dark'],
+  'md-sys-color-tertiary-container':
+    token['md-sys-color-tertiary-container-dark'],
+  'md-sys-color-on-tertiary-container':
+    token['md-sys-color-on-tertiary-container-dark'],
+  'md-sys-color-error': token['md-sys-color-error-dark'],
+  'md-sys-color-on-error': token['md-sys-color-on-error-dark'],
+  'md-sys-color-error-container': token['md-sys-color-error-container-dark'],
+  'md-sys-color-on-error-container':
+    token['md-sys-color-on-error-container-dark'],
+  'md-sys-color-outline': token['md-sys-color-outline-dark'],
+  'md-sys-color-background': token['md-sys-color-background-dark'],
+  'md-sys-color-on-background': token['md-sys-color-on-background-dark'],
+  'md-sys-color-surface': token['md-sys-color-surface-dark'],
+  'md-sys-color-on-surface': token['md-sys-color-on-surface-dark'],
+  'md-sys-color-surface-variant': token['md-sys-color-surface-variant-dark'],
+  'md-sys-color-on-surface-variant':
+    token['md-sys-color-on-surface-variant-dark'],
+  'md-sys-color-inverse-surface': token['md-sys-color-inverse-surface-dark'],
+  'md-sys-color-inverse-on-surface':
+    token['md-sys-color-inverse-on-surface-dark'],
+  'md-sys-color-inverse-primary': token['md-sys-color-inverse-primary-dark'],
+  'md-sys-color-shadow': token['md-sys-color-shadow-dark'],
+  'md-sys-color-surface-tint': token['md-sys-color-surface-tint-dark'],
+  'md-sys-color-outline-variant': token['md-sys-color-outline-variant-dark'],
+  'md-sys-color-scrim': token['md-sys-color-scrim-dark'],
+};
