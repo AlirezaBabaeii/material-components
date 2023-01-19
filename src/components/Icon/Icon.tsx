437f33515cf987abfb9e3fe1f9eface01067754f
mikoloism <mikoloism.github@gmail.com>
2023-01-19 15:41:32 +0330

    feat(components): add icon `enums` and `types`;
    rename `IconName` enum to `IconSymbol`; normalize the icon name from `kebab-case` to `snake_case`;

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,11 +1,16 @@
-import type { IconType, IconKindType } from './types';
+import type { IconSymbolType, IconKindType } from './types';
+
+function normalizeIconSymbols(symbol: IconSymbolType): string {
+	return symbol.replace(/-/g, '_');
+}
 
 type Props = {
-	icon: IconType;
+	icon: IconSymbolType;
 	kind?: IconKindType;
 };
 export default function IconComponent({ icon, kind }: Props): JSX.Element {
-	return <span className={`material-symbols-${kind}`}>{icon}</span>;
+	const iconName: string = normalizeIconSymbols(icon);
+	return <span className={`material-symbols-${kind}`}>{iconName}</span>;
 }
 
 const defaultProps: Omit<Props, 'icon'> = { kind: 'outlined' };
