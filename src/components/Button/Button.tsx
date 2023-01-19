f98c49b0545df9a82eb1c2f5a84d2edf6e4faa61
mikoloism <mikoloism.github@gmail.com>
2023-01-19 18:16:10 +0330

    feat(component): support anchor link on button component;
    button component can be a `anchor link component` by passing `href` property to props of button component; re-order state layer to fix `z-axis` conflict;

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,20 +8,25 @@ type Props = {
 	label: string;
 	icon?: IconSymbolType;
 	variant?: VariantType;
+	href?: string;
 };
 export default function ButtonComponent(props: Props): JSX.Element {
 	const layer = useVariant(props.variant!);
+	const isAnchor = Boolean(props.href);
 
 	return (
-		<layer.Container hasIcon={!!props.icon}>
-			<layer.State />
+		<layer.Container
+			as={isAnchor ? 'a' : 'button'}
+			hasIcon={!!props.icon}>
 			{!!props.icon && <Icon icon={props.icon} />}
 			<layer.Label>{props.label}</layer.Label>
+			<layer.State />
 		</layer.Container>
 	);
 }
 
 const defaultProps: Omit<Props, 'label'> = {
+	href: undefined,
 	icon: undefined,
 	variant: 'filled',
 };
